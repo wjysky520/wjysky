4d6651e1f9687bf1a394e5091d947d47165e5a18
王俊元 <wangjunyuan@talkweb.com.cn>
2023-02-16 16:26:07 +0800

    优化线程池

--- game-service/src/main/java/com/wjysky/config/thread/CardsGameThreadPoolConfig.java
+++ game-service/src/main/java/com/wjysky/config/thread/CardsGameThreadPoolConfig.java
@@ -1,10 +1,14 @@
 package com.wjysky.config.thread;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -17,36 +21,66 @@ import java.util.concurrent.ThreadPoolExecutor;
  */
 @EnableAsync
 @Configuration
-public class CardsGameThreadPoolConfig {
+@Slf4j
+public class CardsGameThreadPoolConfig implements AsyncConfigurer {
 
-//    @Value("${task.pool.corePoolSize}")
-    private int corePoolSize; // 核心线程数
+    /**
+     * 核心线程数，IO密集型
+     */
+    public static final int CORE_POOL_SIZE = Runtime.getRuntime().availableProcessors() * 2;
 
-//    @Value("${task.pool.maxPoolSize}")
-    private int maxPoolSize; // 最大线程数
+    /**
+     * 最大工作线程数
+     */
+    public static final int MAX_POOL_SIZE = Runtime.getRuntime().availableProcessors() * 5;
 
-//    @Value("${task.pool.queueCapacity}")
-    private int queueCapacity; // 队列中最大的数目
+    /**
+     * 允许线程空闲时间（单位为秒）
+     */
+    public static final int KEEP_ALIVE_TIME = 10;
 
-//    @Value("${task.pool.keepAliveSeconds}")
-    private int keepAliveSeconds; // 线程空闲后的最大存活时间
+    /**
+     * 缓冲队列数
+     */
+    public static final int QUEUE_CAPACITY = Runtime.getRuntime().availableProcessors() * 2;
+
+    /**
+     * 线程池中任务的等待时间，如果超过这个时候还没有销毁就强制销毁
+     */
+    public static final int AWAIT_TERMINATION = 60;
+
+    /**
+     * 用来设置线程池关闭的时候等待所有任务都完成再继续销毁其他的Bean
+     */
+    public static final Boolean WAIT_COMPLETE_ON_SHUTDOWN = true;
+
+    /**
+     * 线程池名前缀
+     */
+    public static final String THREAD_NAME_PREFIX = "Async-Service-";
 
     @Bean("cardsGameTaskExecutor")
-    public Executor taskExecutor() {
+    public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        corePoolSize = Runtime.getRuntime().availableProcessors(); //CPU密集型，因此用CPU核心数
-        executor.setCorePoolSize(corePoolSize);
-        maxPoolSize = corePoolSize * 4;
-        executor.setMaxPoolSize(maxPoolSize);
-        queueCapacity = 1024;
-        executor.setQueueCapacity(queueCapacity);
-        keepAliveSeconds = 60;
-        executor.setKeepAliveSeconds(keepAliveSeconds);
-        executor.setThreadNamePrefix("CardsGameTaskExecutor-"); // 线程名称前缀
-        // setRejectedExecutionHandler：当pool已经达到max size的时候，如何处理新任务
-        // CallerRunsPolicy：不在新线程中执行任务，而是由调用者所在的线程来执行
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setKeepAliveSeconds(KEEP_ALIVE_TIME);
+        executor.setQueueCapacity(QUEUE_CAPACITY);
+        executor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        executor.setWaitForTasksToCompleteOnShutdown(WAIT_COMPLETE_ON_SHUTDOWN);
+        executor.setAwaitTerminationSeconds(AWAIT_TERMINATION);
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (Throwable ex, Method method, Object... params) -> {
+            log.error("class#method: " + method.getDeclaringClass().getName() + "#" + method.getName());
+            log.error("type        : " + ex.getClass().getName());
+            log.error("exception   : " + ex.getMessage());
+            log.error("错误信息   : ", ex);
+        };
     }
 }
